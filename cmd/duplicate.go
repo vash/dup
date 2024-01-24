2bd252ed5be0c031dce09fbceb60f2b31da55d74
Vash <admin@vash.dev>
2024-01-24 16:20:39 +0200

    feat: wip

--- cmd/duplicate.go
+++ cmd/duplicate.go
@@ -7,29 +7,10 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/kubernetes"
 )
 
-func CreateDuplicatePod(ctx context.Context, ioStreams genericclioptions.IOStreams, client *kubernetes.Clientset, deploymentName, namespace, duplicateName string, edit bool) {
-	var err error
-	clonedPod := clonePod(client, deploymentName, namespace, duplicateName)
-	clonedPod.Spec.RestartPolicy = "Never"
-
-	if err != nil {
-		fmt.Printf("Error editing pod: %v\n", err)
-		os.Exit(1)
-	}
-	_, err = client.CoreV1().Pods(namespace).Create(ctx, clonedPod, metav1.CreateOptions{})
-	if err != nil {
-		fmt.Printf("Error creating cloned deployment: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("Cloned deployment created successfully.")
-}
-
-func clonePod(client *kubernetes.Clientset, deploymentName, namespace, duplicateName string) *corev1.Pod {
+func ClonePod(client *kubernetes.Clientset, deploymentName, namespace, duplicateName string) *corev1.Pod {
 	var clonedPod corev1.Pod
 
 	originalDeployment, err := client.AppsV1().Deployments(namespace).Get(context.TODO(), deploymentName, metav1.GetOptions{})
@@ -45,6 +26,6 @@ func clonePod(client *kubernetes.Clientset, deploymentName, namespace, duplicate
 	}
 	clonedPod.ObjectMeta.Labels = labelMap
 	clonedPod.Name = duplicateName
-
+	clonedPod.Spec.RestartPolicy = "Never"
 	return &clonedPod
 }
